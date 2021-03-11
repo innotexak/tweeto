9baa622590f40fcef353eaf30a139642016d67fb
Collins Abadaike <akuhinnocent2016@gmail.com>
2021-03-11 12:57:48 +0100

    The very first commit

---
+++ mytweet/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from  django.http import HttpResponse, Http404, JsonResponse
+from .models import Tweet
+from django.utils.http import is_safe_url
+from django.conf import settings
+import random
+from .forms import TweetForm
+
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
+
+def home_view(request, *args, **kwargs):
+    
+    return render(request, 'pages/home.html', status = 200)
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweet_list =[x.serialize() for x in qs]
+    data = {
+        "isUser": False,
+        "response": tweet_list
+    }
+    return JsonResponse(data)
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    data={
+        "id":tweet_id,
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data["content"] = obj.content 
+    except:
+        data["message"]= "Not Found"
+        status = 404
+    return JsonResponse(data, status=status)
+
+def tweet_form_view(request, *args, **Kwargs):
+    form = TweetForm(request.POST or None)
+    next_url = request.POST.get("next") or None
+    if form.is_valid():
+        data = form.save(commit=False)
+        data.save()
+        if request.is_ajax():
+            return JsonResponse(data.serialize(), status=201)
+        if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
+            return redirect(next_url)
+        form = TweetForm()
+    if form.errors:
+        if request.is_ajax():
+            return JsonResponse(form.errors, status=400)
+    return render(request, 'pages/tweet_form.html', context={"form":form})
